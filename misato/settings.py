23857456c1baa6616f2c98253bf043c51e51c81e
dieselpoweredkitten <me@require.pm>
2013-11-13 06:34:43 +0400

    Update output path

--- misato/settings.py
+++ misato/settings.py
@@ -1,6 +1,6 @@
 OFFICE_LISTENER = ('localhost', 2002)
 
 UPLOAD_PATH = '/tmp/documents/'
-OUTPUT_PATH = '/tmp/documents/output/'
+OUTPUT_PATH = '/tmp/html/'
 MEDIA_URL = 'docs.example.com'
 # http://docs.example.com/{uuid}/ => file:///tmp/docs/output/{uuid}/
