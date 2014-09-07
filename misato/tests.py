79808c49394c0b3e0ff6337f7d0da450eedcd3c6
tyrannosaurus <trex@require.pm>
2014-09-07 20:39:31 +0400

    Add tests for doc/docx documents and create_metadata function

--- misato/tests.py
+++ misato/tests.py
@@ -1,29 +1,57 @@
+import os
+import os.path
+
 import magic
 import misato
-import os.path
 import unittest
 
 
-class ConvertingTestCase(unittest.TestCase):
+class MisatoTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.pdf = misato.convert_to_pdf("samples/sample.odt")
 
     def test_convert_to_pdf(self):
-        pdf = misato.convert_to_pdf("samples/sample.odt")
         with magic.Magic(mimetype=True) as m:
-            mimetype = m.from_file(pdf)
+            mimetype = m.from_file(self.pdf)
             self.assertEqual(mimetype, "application/pdf")
 
     def test_convert_to_png(self):
-        pdf = misato.convert_to_pdf("samples/sample.odt")
-        images = misato.convert_to_png(pdf)
-        self.assertTrue(images.startswith("/tmp/"))
+        previews = misato.convert_to_png(self.pdf)
+        self.assertTrue(previews.startswith("/tmp/"))
         with magic.Magic(mimetype=True) as m:
-            mimetype = m.from_file(os.path.join(images, "0.png"))
+            mimetype = m.from_file(os.path.join(previews, "0.png"))
             self.assertEqual(mimetype, "image/png")
 
-    def test_convert(self):
+    def test_create_metadata(self):
+        previews = misato.convert_to_png(self.pdf)
+        metadata = misato.create_metadata(previews)
+        self.assertTrue(isinstance(metadata, dict))
+        self.assertEqual(metadata.get("pages_count"), 1)
+
+    def test_convert_odt(self):
         stream = misato.return_stream("samples/sample.odt")
         response = misato.convert(stream)
         self.assertEqual(next(response), {"pages_count": 1})
         with magic.Magic(mimetype=True) as m:
             mimetype = m.from_buffer(next(response))
             self.assertEqual(mimetype, "application/gzip")
+
+    def test_convert_doc(self):
+        stream = misato.return_stream("samples/sample.doc")
+        response = misato.convert(stream)
+        self.assertEqual(next(response), {"pages_count": 1})
+        with magic.Magic(mimetype=True) as m:
+            mimetype = m.from_buffer(next(response))
+            self.assertEqual(mimetype, "application/gzip")
+
+    def test_convert_docx(self):
+        stream = misato.return_stream("samples/sample.docx")
+        response = misato.convert(stream)
+        self.assertEqual(next(response), {"pages_count": 1})
+        with magic.Magic(mimetype=True) as m:
+            mimetype = m.from_buffer(next(response))
+            self.assertEqual(mimetype, "application/gzip")
+
+    def tearDown(self):
+        os.remove(self.pdf)
