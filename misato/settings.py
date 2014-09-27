28777110696349ab9091386a5225a8a100a633e0
tyrannosaurus <trex@require.pm>
2014-09-27 19:26:39 +0400

    Add more supported mimetypes

--- misato/settings.py
+++ misato/settings.py
@@ -1,7 +1,14 @@
 LIBREOFFICE_PATH = "/usr/lib/libreoffice/program"
 ALLOWED_MIMETYPES = (
-    "application/pdf",
+    "text/plain",
+    "application/pdf", # Adobe PDF
     "application/msword", # Microsoft Word 2003
     "application/vnd.openxmlformats-officedocument.wordprocessingml.document", # Microsoft Word 2007
-    "application/vnd.oasis.opendocument.text", # LibreOffice ODT
+    "application/vnd.ms-excel", # Microsoft Excel 2003
+    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", # Microsoft Excel 2007
+    "application/vnd.ms-powerpoint", # Microsoft PowerPoint 2003
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation", # Microsoft PowerPoint 2007
+    "application/vnd.oasis.opendocument.text", # LibreOffice Writer
+    "application/vnd.oasis.opendocument.spreadsheet", # LibreOffice Calc
+    "application/vnd.oasis.opendocument.presentation", # LibreOffice Impress
 )
